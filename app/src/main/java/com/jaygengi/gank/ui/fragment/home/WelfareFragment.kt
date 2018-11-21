890370b569b73ec6faf87718bdfa8fa3d2bcd9d4
JayGengi <jaygengi@163.com>
2018-11-21 11:19:45 +0800

    大图查看器

--- app/src/main/java/com/jaygengi/gank/ui/fragment/home/WelfareFragment.kt
+++ app/src/main/java/com/jaygengi/gank/ui/fragment/home/WelfareFragment.kt
@@ -1,6 +1,7 @@
 package com.jaygengi.gank.ui.fragment.home
 
 import android.support.v7.widget.GridLayoutManager
+import cc.shinichi.library.ImagePreview
 import com.jaygengi.gank.R
 import com.jaygengi.gank.base.BaseFragment
 import com.jaygengi.gank.mvp.contract.GirlsContract
@@ -9,6 +10,11 @@ import com.jaygengi.gank.mvp.presenter.GirlsPresenter
 import com.jaygengi.gank.net.exception.ErrorStatus
 import com.jaygengi.gank.ui.adapter.GirlsAdapter
 import kotlinx.android.synthetic.main.fragment_home_common_type.*
+import cc.shinichi.library.bean.ImageInfo
+
+
+
+
 
 /**
    * @description: 福利
@@ -19,7 +25,9 @@ import kotlinx.android.synthetic.main.fragment_home_common_type.*
 
 class WelfareFragment : BaseFragment(), GirlsContract.View {
     private var isRefresh = false
+    private val imageInfoList: ArrayList<ImageInfo> = ArrayList()
 
+    private var imageInfo = ImageInfo()
     private val mPresenter by lazy { GirlsPresenter() }
     private var grilsList = ArrayList<GirlsEntity.ResultsBean>()
     private val mAdapter by lazy { activity?.let { GirlsAdapter( grilsList) } }
@@ -43,9 +51,15 @@ class WelfareFragment : BaseFragment(), GirlsContract.View {
             layoutManager = GridLayoutManager(context,2)
             adapter = mAdapter
         }
-//        mAdapter!!.setOnItemClickListener { adapter, _, position ->
-//            val item :CategoryEntity.ResultsBean = adapter.getItem(position) as CategoryEntity.ResultsBean
-//        }
+        mAdapter!!.setOnItemClickListener { adapter, _, position ->
+            ImagePreview
+                    .getInstance()
+                    .setContext(context!!)
+                    .setIndex(position)// 从第一张图片开始，索引从0开始哦
+                    .setFolderName("Gank")// 保存的文件夹名称，SD卡根目录
+                    .setImageInfoList(imageInfoList)
+                    .start()
+        }
     }
 
     override fun lazyLoad() {
@@ -62,8 +76,15 @@ class WelfareFragment : BaseFragment(), GirlsContract.View {
             if (dataInfo.results != null && dataInfo.results!!.isNotEmpty()) {
                 if (CURRENT_PAGE == 1) {
                     grilsList.clear()
+                    imageInfoList.clear()
                 }
                 grilsList.addAll(dataInfo.results!!)
+                for(img : GirlsEntity.ResultsBean in grilsList){
+                    imageInfo = ImageInfo()
+                    imageInfo.originUrl = img.url
+                    imageInfo.thumbnailUrl = img.url
+                    imageInfoList.add(imageInfo)
+                }
                 setNewData(grilsList)
             } else {
                 multipleStatusView?.showEmpty()
